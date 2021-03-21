619d3791564b6d2d0da1b2d16970e60c511953f1
Duncan Eddy <duncan.eddy@gmail.com>
2021-03-21 12:04:07 -0700

    Test version bumping

--- scripts/set_version.sh
+++ scripts/set_version.sh
@@ -5,14 +5,14 @@
 #   Script to help set package version across 
 #
 # Usage: 
-#   $ ./set_version.sh 0.1.0
+#   $ ./set_version.sh 0.1.0 0.1.1
 # 
 
 # Replace worspace specific dependencies and versions in lockstep
-sed -i.bak "s/version = (0-9).(0-9).(0.9)/version = \"$1\"/" ./rastro/Cargo.toml
-sed -i.bak "s/version = (0-9).(0-9).(0.9)$/version = \"$1\"/" ./rastro_python/Cargo.toml
-sed -i.bak "s/version = \"(0-9).(0-9).(0.9)\" }$/version = \"\^$1\" \}/" ./rastro_python/Cargo.toml
+sed -i.bak "s/version = \"$1\"/version = \"$2\"/" ./rastro/Cargo.toml
+sed -i.bak "s/version = \"$1\"$/version = \"$2\"/" ./rastro_python/Cargo.toml
+sed -i.bak "s/version = \"$1\" }$/version = \"$2\" \}/" ./rastro_python/Cargo.toml
 
 # Remove tempoarary files 
-rm ./rust_python_lib/Cargo.toml.bak
-rm ./rust_python_module/Cargo.toml.bak
+rm ./rastro/Cargo.toml.bak
+rm ./rastro_python/Cargo.toml.bak
