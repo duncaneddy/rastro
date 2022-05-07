5ab0358498f3bce7027301ccdba705da2b7e5808
Duncan Eddy <duncan.eddy@gmail.com>
2022-05-07 09:23:22 -0700

    Fix bad uncomment to fix build

--- rastro_python/src/lib.rs
+++ rastro_python/src/lib.rs
@@ -1110,14 +1110,14 @@ fn rotation_eci_to_ecef<'py>(py: Python<'py>, epc: &Epoch) -> &'py PyArray<f64, 
 ///
 /// Returns:
 ///     r (`numpy.ndarray`): 3x3 Rotation matrix transforming ITRF -> GCRF
-// #[pyfunction]
-// #[pyo3(text_signature = "(epc)")]
-// fn rotation_ecef_to_eci<'py>(py: Python<'py>, epc: &Epoch) -> &'py PyArray<f64, Ix2> {
-//     unsafe {
-//         let mat = frames::rotation_ecef_to_eci(epc.obj);
-//         matrix_to_numpy!(py, mat, 3, 3, f64)
-//     }
-// }
+#[pyfunction]
+#[pyo3(text_signature = "(epc)")]
+fn rotation_ecef_to_eci<'py>(py: Python<'py>, epc: &Epoch) -> &'py PyArray<f64, Ix2> {
+    unsafe {
+        let mat = frames::rotation_ecef_to_eci(epc.obj);
+        matrix_to_numpy!(py, mat, 3, 3, f64)
+    }
+}
 
 /////////////////////
 // Transformations //
